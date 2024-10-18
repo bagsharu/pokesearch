8569dfe6a181de49ce75534b7be873a6fde52e6a
Haru <gabs.haru@gmail.com>
2024-10-17 21:43:22 -0400

    Tweaked Record to properly gather info on pokemon's type

--- src/main/java/bagsharu/pokesearch/model/PokemonInfo.java
+++ src/main/java/bagsharu/pokesearch/model/PokemonInfo.java
@@ -4,7 +4,21 @@ package bagsharu.pokesearch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record PokemonInfo(@JsonAlias("name") String pokeName,
-                          @JsonAlias("weight") String pokeWeight) {
+                          @JsonAlias("weight") String pokeWeight,
+                          @JsonAlias("types") List<pokeTypeSlot> pokeType) {
+}
+
+
+record pokeTypeSlot (int slot,
+                     TypeInfo type){
+
+}
+
+record TypeInfo (String name,
+                 String url) {
+
 }
