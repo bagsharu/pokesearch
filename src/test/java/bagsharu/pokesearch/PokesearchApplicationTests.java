baaaf4a7860ed1764b5eee8d44493ea4a54f86bc
Haru <gabs.haru@gmail.com>
2024-10-17 20:05:14 -0400

    First Commit with Jackson dependency included

---
+++ src/test/java/bagsharu/pokesearch/PokesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package bagsharu.pokesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
