14a1e20ed5c045796110f4910f9b1a96d80c6384
Haru <gabs.haru@gmail.com>
2024-10-31 11:31:30 -0400

    Autowired repository for Spring

--- src/main/java/bagsharu/pokesearch/PokesearchApplication.java
+++ src/main/java/bagsharu/pokesearch/PokesearchApplication.java
@@ -2,8 +2,10 @@ package bagsharu.pokesearch;
 
 import bagsharu.pokesearch.main.Principal;
 import bagsharu.pokesearch.model.PokemonInfo;
+import bagsharu.pokesearch.repository.PokemonRepository;
 import bagsharu.pokesearch.service.ApiRequest;
 import bagsharu.pokesearch.service.ConvertData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PokesearchApplication implements CommandLineRunner {
 
+	@Autowired
+	private PokemonRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(PokesearchApplication.class, args);
 	}
@@ -18,7 +22,7 @@ public class PokesearchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Principal principal = new Principal();
+		Principal principal = new Principal(repository);
 
 		principal.SearchPokemon();
 	}
