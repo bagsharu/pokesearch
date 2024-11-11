a1a607e33808b97a60ad113196b30467843f2d4b
Haru <gabs.haru@gmail.com>
2024-11-11 12:56:26 -0400

    New repository method

--- src/main/java/bagsharu/pokesearch/repository/PokemonRepository.java
+++ src/main/java/bagsharu/pokesearch/repository/PokemonRepository.java
@@ -3,8 +3,11 @@ package bagsharu.pokesearch.repository;
 import bagsharu.pokesearch.model.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
     Optional<Pokemon> findByPokemonNameContainingIgnoreCase(String pokeName);
+
+    List<Pokemon> findByPokemonTypeMainContainingIgnoreCase(String typeSearch);
 }
