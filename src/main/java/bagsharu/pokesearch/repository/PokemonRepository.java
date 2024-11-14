090c2c1ba5b72d205d4ca53952b54a0d48fe10cb
bagsharu <gabs.haru@gmail.com>
2024-11-14 19:15:26 -0400

    Removed unused method and add comments

--- src/main/java/bagsharu/pokesearch/repository/PokemonRepository.java
+++ src/main/java/bagsharu/pokesearch/repository/PokemonRepository.java
@@ -9,10 +9,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+
+    // This method simply search the databse for a pokemon with the desired name
     Optional<Pokemon> findByPokemonNameContainingIgnoreCase(String pokeName);
 
-    List<Pokemon> findByPokemonTypeMainContainingIgnoreCase(String typeSearch);
 
+    // This method searches the column of the pokemon types and checks if they contain the type asked
     @Query("SELECT p FROM Pokemon p WHERE p.pokemonTypeMain LIKE %:typeSearch% OR p.pokemonTypeSecond LIKE %:typeSearch%")
     List<Pokemon> searchByType(@Param("typeSearch") String param);
 
