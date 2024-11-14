3a931523b748bb6b5b6d5a9cdf6a4e45329256b7
bagsharu <gabs.haru@gmail.com>
2024-11-14 19:09:08 -0400

    Query method for type search

--- src/main/java/bagsharu/pokesearch/repository/PokemonRepository.java
+++ src/main/java/bagsharu/pokesearch/repository/PokemonRepository.java
@@ -2,6 +2,8 @@ package bagsharu.pokesearch.repository;
 
 import bagsharu.pokesearch.model.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,8 @@ public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
     Optional<Pokemon> findByPokemonNameContainingIgnoreCase(String pokeName);
 
     List<Pokemon> findByPokemonTypeMainContainingIgnoreCase(String typeSearch);
+
+    @Query("SELECT p FROM Pokemon p WHERE p.pokemonTypeMain LIKE %:param% OR p.pokemonTypeSecond LIKE %:param%")
+    List<Pokemon> searchByType(@Param("param") String param);
+
 }
